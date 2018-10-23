a3e0a33826b42200a189690a0e5a608692abdc0b
Ajay Saini <ajcse1@gmail.com>
2018-10-22 17:50:08 -0700

    PEP8 corrections for rrtm_clr.py

--- jaws/rigb/rrtm_clr.py
+++ jaws/rigb/rrtm_clr.py
@@ -1,29 +1,24 @@
+import csv
+from datetime import datetime, timedelta
+import glob
+import os
+
 import climlab
+from climlab.domain.field import Field
+from climlab.domain import domain
 import numpy as np
 import pandas as pd
 import xarray as xr
 
-import csv
-import os
-import glob
-import sys
-from datetime import datetime, timedelta
-
 import sunposition
 
-import subprocess
-
-from climlab.domain.field import Field
-from climlab.domain import domain
-
 '''
-Run RRTM using processed AIRS data from nomiss_airs_lvl3_rrtm.ncl
+Run RRTM using processed AIRS data from nomiss_airs.py
 - run 24 hour twice (A and D)
 - cal SZA
 - output netCDF
-
-Prototype: rrtm_try.py
 '''
+
 
 def make_column(lev=None,  ps=1013, tmp=None, ts=None):
     state = climlab.column_state(lev=lev)
@@ -45,34 +40,32 @@ def make_column(lev=None,  ps=1013, tmp=None, ts=None):
     
     return state
 
+
 def main():
     indir = "nomiss-airx3std/"
     outdir = "rrtm-airx3std/"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
-
     # constants
-    xpt = '.airx3std.'
     alb = 0.75
     emis = 0.985
     solar_constant = 1367.0
 
-    absorber_vmr = {}
-    absorber_vmr['CO2']   = 355. / 1E6
-    absorber_vmr['CH4']   = 1714. / 1E9
-    absorber_vmr['N2O']   = 311. / 1E9
-    absorber_vmr['O2']    = 0.21
+    absorber_vmr = dict()
+    absorber_vmr['CO2'] = 355. / 1E6
+    absorber_vmr['CH4'] = 1714. / 1E9
+    absorber_vmr['N2O'] = 311. / 1E9
+    absorber_vmr['O2'] = 0.21
     absorber_vmr['CFC11'] = 0.280/1E9
     absorber_vmr['CFC12'] = 0.503/1E9
     absorber_vmr['CFC22'] = 0.
-    absorber_vmr['CCL4']  = 0.
+    absorber_vmr['CCL4'] = 0.
 
     # knob
     aod = np.zeros((6, 1, 18))
-    aod[1,0,-3:] = 0.12/3
-    aod[5,0,:7] = 0.0077/7
-
+    aod[1, 0, -3:] = 0.12/3
+    aod[5, 0, :7] = 0.0077/7
 
     # stn names and lat/lon
     lst_stn = pd.read_csv('stn_gp_jaws_edited.txt')
@@ -88,12 +81,11 @@ def main():
 
     year = '2009'
     month = '12'
-    days = ['01','02','03','04','05','06','07','08','09','10',
-    '11','12','13','14','15','16','17','18','19','20',
-    '21','22','23','24','25','26','27','28','29','30','31']
+    days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
-
-    #main function
+    # main function
     for i in range(nstn):
         stn = stn_names[i]
         lat_deg = latstn[i]
@@ -115,17 +107,17 @@ def main():
                         for hr in range(24):
                             dtime = datetime.strptime(fn.split('_')[1], "%Y%m%d") + timedelta(hours=hr)
 
-                            #fo = outdir+stn+'_'+dtime.strftime('%Y%m%d:%H')+fn[-5:]
+                            # fo = outdir+stn+'_'+dtime.strftime('%Y%m%d:%H')+fn[-5:]
                             fo = outdir+stn+'_'+dtime.strftime('%Y%m%d')+'.txt'
 
                             sza = sunposition.sunpos(dtime, lat_deg, lon_deg, 0)[1]
                             cossza = np.cos(np.radians(sza))
 
                             tmp = fin['t'].values
-                            #ts = fin['sfc_tmp'].values
+                            # ts = fin['sfc_tmp'].values
                             ts = fin['ts'].values
                             plev = fin['plev'].values
-                            #ps = fin['sfc_prs'].values
+                            # ps = fin['sfc_prs'].values
                             ps = fin['ps'].values
 
                             state = make_column(lev=plev, ps=ps, tmp=tmp, ts=ts)
@@ -135,34 +127,36 @@ def main():
 
                             h2o_q = fin['q'].values
 
-                            rad = climlab.radiation.RRTMG(name='Radiation', state=state, specific_humidity=h2o_q, albedo=alb, coszen=cossza, absorber_vmr=absorber_vmr, emissivity=emis, S0=solar_constant, icld=0)
+                            rad = climlab.radiation.RRTMG(name='Radiation', state=state, specific_humidity=h2o_q,
+                                                          albedo=alb, coszen=cossza, absorber_vmr=absorber_vmr,
+                                                          emissivity=emis, S0=solar_constant, icld=0)
                             rad.compute_diagnostics()
 
                             dout = rad.to_xarray(diagnostics=True)
                             sw_dn.append(dout['SW_flux_down_clr'].values[-1])
-                            #break
-                            #dout.to_netcdf(fo)
+                            # break
+                            # dout.to_netcdf(fo)
                 
                 count = 0
                 while count < 24:
-                    #sw_dn_final[count] = [(g+h)/2.0 for g,h in zip(sw_dn[count], sw_dn[count+24])]
+                    # sw_dn_final[count] = [(g+h)/2.0 for g,h in zip(sw_dn[count], sw_dn[count+24])]
                     sw_dn_final[count] = (sw_dn[count]+sw_dn[count+24])/2.0
                     count += 1
 
                 with open(fo, 'w') as outfile:
                     wr = csv.writer(outfile)
                     wr.writerow(sw_dn_final)
-                #print(fo)
-                #np.savetxt(fo, sw_dn_final, delimiter=",", fmt='%s')
+                # print(fo)
+                # np.savetxt(fo, sw_dn_final, delimiter=",", fmt='%s')
 
-                #outfile = open(fo, 'w')
-                #myString = ",".join(map(str,sw_dn_final))
-                #outfile.write("%s," % myString)
+                # outfile = open(fo, 'w')
+                # myString = ",".join(map(str,sw_dn_final))
+                # outfile.write("%s," % myString)
                 
-                #for item in sw_dn_final:
+                # for item in sw_dn_final:
                 #    outfile.write("%s," % item)
 
-                #break
+                # break
             except:
                 pass
 
