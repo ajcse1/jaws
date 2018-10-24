8d5b585f66d7cb5cb238437bd2aa019690986c95
Ajay Saini <ajcse1@gmail.com>
2018-10-24 15:52:24 -0700

    PEP8 corrections for tilt_angle.py

--- jaws/rigb/tilt_angle.py
+++ jaws/rigb/tilt_angle.py
@@ -1,14 +1,14 @@
+from datetime import datetime
+
+import Ngl
 import numpy as np
 import pandas as pd
-import xarray as xr
-import Ngl
+
 import sunposition
-from datetime import datetime
-from math import sin, cos
-import csv
+
 try:
     from itertools import izip as zip
-except ImportError: #Python 3.x
+except ImportError:  # Python 3.x
     pass
 
 
@@ -18,7 +18,6 @@ def deg_to_rad(list_deg):
     return list_rad
 
 
-#def main():
 def main(dataset, latitude, longitude):
     ddr = 0.25
     rho = 0.8
@@ -68,18 +67,17 @@ def main(dataset, latitude, longitude):
 
         hours_nonmsng = list(range(len(fsds_jaws_nonmsng)))
 
-        #Interpolate fsds and sza for half-hour values
+        # Interpolate fsds and sza for half-hour values
         fsds_intrp = list(Ngl.ftcurv(hours_nonmsng, fsds_jaws_nonmsng, half_hours))
 
-        
-        #Calculate azimuth angle
-        az=[]
-        sza=[]
+        # Calculate azimuth angle
+        az = []
+        sza = []
         for hour in hours:
-            dtime = datetime(year,month,day,hour,0)
+            dtime = datetime(year, month, day, hour, 0)
             az.append(sunposition.sunpos(dtime, lat, lon, 0)[0])
             sza.append(sunposition.sunpos(dtime, lat, lon, 0)[1])
-            dtime = datetime(year,month,day,hour,30)
+            dtime = datetime(year, month, day, hour, 30)
             az.append(sunposition.sunpos(dtime, lat, lon, 0)[0])
             sza.append(sunposition.sunpos(dtime, lat, lon, 0)[1])
 
@@ -87,29 +85,28 @@ def main(dataset, latitude, longitude):
 
         alpha = [(90-i) for i in sza]
 
-        beta = list(np.arange(0.25,45.25,0.25))
+        beta = list(np.arange(0.25, 45.25, 0.25))
 
-        sza_noon = [cos(np.radians(i)) for i in sza]
+        sza_noon = [np.cos(np.radians(i)) for i in sza]
 
-        #Check if measured solar noon time > true solar noon time
+        # Check if measured solar noon time > true solar noon time
         if fsds_intrp.index(max(fsds_intrp)) > sza_noon.index(max(sza_noon)):
-            aw = list(np.arange(0,180,0.25))
+            aw = list(np.arange(0, 180, 0.25))
         else:
-            aw = list(np.arange(-179.75,0.25,0.25))
+            aw = list(np.arange(-179.75, 0.25, 0.25))
 
         az = deg_to_rad(az)
         alpha = deg_to_rad(alpha)
         beta = deg_to_rad(beta)
         aw = deg_to_rad(aw)
 
-        #Make pairs of aw,beta
+        # Make pairs of aw,beta
         pairs = []
         for i in aw:
             for j in beta:
-                pairs.append(tuple((i,j)))
+                pairs.append(tuple((i, j)))
 
-
-        #Find all possible pairs using correct fsds
+        # Find all possible pairs using correct fsds
         possible_pairs = []
         daily_avg_diff = []
         best_pairs = []
@@ -120,9 +117,10 @@ def main(dataset, latitude, longitude):
             cos_i = []
             fsds_correct = []
             while count < len(alpha):
-                cos_i.append((cos(alpha[count])*cos(az[count]-pair[0])*sin(pair[1])+(sin(alpha[count])*cos(pair[1]))))
-                nmr = fsds_intrp[count]*(sin(alpha[count])+ddr)
-                dnmr = cos_i[count]+(ddr*(1+cos(pair[1]))/2.)+(rho*(sin(alpha[count])+ddr)*(1-cos(pair[1]))/2.)
+                cos_i.append((np.cos(alpha[count])*np.cos(az[count]-pair[0])*np.sin(pair[1])+(
+                        np.sin(alpha[count])*np.cos(pair[1]))))
+                nmr = fsds_intrp[count]*(np.sin(alpha[count])+ddr)
+                dnmr = cos_i[count]+(ddr*(1+np.cos(pair[1]))/2.)+(rho*(np.sin(alpha[count])+ddr)*(1-np.cos(pair[1]))/2.)
                 if dnmr == 0:
                     dnmr = smallest_double
                 fsds_correct.append(nmr/dnmr)
@@ -140,11 +138,12 @@ def main(dataset, latitude, longitude):
                     fsds_correct_half.pop(msng_idx)
                     fsds_rrtm.pop(msng_idx)
 
-                diff = [abs(x-y) for x,y in zip(fsds_correct_half[clrhr_start:clrhr_end], fsds_rrtm[clrhr_start:clrhr_end])]
+                diff = [abs(x-y) for x,y in zip(fsds_correct_half[clrhr_start:clrhr_end],
+                                                fsds_rrtm[clrhr_start:clrhr_end])]
                 daily_avg_diff.append(np.nanmean(diff))
 
-        #print(day)
-        print(len(possible_pairs))
+        # print(day)
+        # print(len(possible_pairs))
 
         #########PART-2#############
 
@@ -155,7 +154,7 @@ def main(dataset, latitude, longitude):
                 if val <= min(dailyavg_possiblepair_dict.keys())+5:
                     best_pairs.append(dailyavg_possiblepair_dict.get(val))
 
-        print(len(best_pairs))
+        # print(len(best_pairs))
 
         #########PART-3#############
  
