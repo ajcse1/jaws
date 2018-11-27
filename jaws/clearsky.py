Correct calculation of dates and use relative path for para_file

jaws/clearsky.py
@@ -7,6 +7,11 @@ import numpy as np
 import pandas as pd
 import scipy.interpolate
 
+try:
+    from jaws import common
+except:
+    import common
+
 
 def interpolate(x, y):
     dv = scipy.interpolate.interp1d(x, y, fill_value='extrapolate')
@@ -24,7 +29,8 @@ def interpolate(x, y):
 
 
 def clr_prd(dat_sza, tg_fsds, tg_sza, date, stn_name):
-    para_file = pd.read_csv('../resources/lst_para_rdn.txt')
+    path = common.relative_path('resources/lst_para_rdn.txt')
+    para_file = pd.read_csv(path)
 
     scale = para_file.loc[para_file['stn_name'] == stn_name, 'scale'].iloc[0]
     offset = para_file.loc[para_file['stn_name'] == stn_name, 'offset'].iloc[0]
@@ -87,7 +93,7 @@ def write_to_file(cons_clr_hrs, daylight, date):
 
     if len(daylight) <= 12:
         min_clrhrs_needed = 6
-    elif 12 <= len(daylight) <= 16:
+    elif 12 < len(daylight) < 16:
         min_clrhrs_needed = 7
     elif len(daylight) >= 16:
         min_clrhrs_needed = 8
@@ -110,12 +116,12 @@ def main(dataset):
     ds = dataset.drop('time_bounds')
     df = ds.to_dataframe()
 
-    stn_name = df['station_name'][0]
-
-    df.reset_index(level=['time'], inplace=True)
-    date_hour = df['time'].tolist()
+    date_hour = [datetime.utcfromtimestamp(i) for i in df.index.values]
     dates = [i.date() for i in date_hour]
     dates = sorted(set(dates), key=dates.index)
+
+    df.reset_index(level=['time'], inplace=True)
+    stn_name = df['station_name'][0]
 
     for date in dates:
         df_temp = df[(df.year == date.year) & (df.month == date.month) & (df.day == date.day)]