97aca75852aeeb9dc302787bf49f8cf7ddf683f6
Ajay Saini <ajcse1@gmail.com>
2018-11-28 17:03:27 -0800

    fsds_all.py: Correct calculation of dates

--- jaws/fsds_all.py
+++ jaws/fsds_all.py
@@ -1,9 +1,15 @@
+from datetime import datetime
 import os
 import requests
 
 import numpy as np
 import pandas as pd
 import xarray as xr
+
+try:
+    from jaws import common
+except:
+    import common
 
 
 def deg_to_rad(list_deg):
@@ -12,39 +18,39 @@ def deg_to_rad(list_deg):
     return list_rad
 
 
-def main(dataset):
+def main(dataset, args):
     rho = 0.8  # reflectance
     smallest_double = 2.2250738585072014e-308
     fillvalue_double = 9.969209968386869e+36
+    dtime_1970, tz = common.time_common(args.tz)
+    idx_count = 0
 
     ds = dataset
     ds = ds.drop('time_bounds')
     df = ds.to_dataframe()
 
+    date_hour = [datetime.fromtimestamp(i, tz) for i in df.index.values]
+    dates = [i.date() for i in date_hour]
+    dates = sorted(set(dates), key=dates.index)
+
     df['fsds_corrected'] = ''
 
     df.reset_index(level=['time'], inplace=True)
-    idx_count = 0
-
-    dates = df['time'].tolist()
-    dates = pd.to_datetime(dates)
-    dates = [i.date() for i in dates]
-    dates = sorted(set(dates), key=dates.index)
-
     stn = df['station_name'][0]
 
     grele_path = 'http://grele.ess.uci.edu/jaws/rigb_data/'
     dir_ceres = 'ceres/'
-    url = grele_path + dir_ceres + stn + '.ceres.nc'
+    sfx = '.ceres.nc'
+    url = grele_path + dir_ceres + stn + sfx
     r = requests.get(url, allow_redirects=True)
-    open(stn + '.ceres.nc', 'wb').write(r.content)
+    open(stn + sfx, 'wb').write(r.content)
 
     for date in dates:
         year = date.year
         month = date.month
         day = date.day
 
-        ceres_df = xr.open_dataset(stn + '.ceres.nc').to_dataframe()
+        ceres_df = xr.open_dataset(stn + sfx).to_dataframe()
         cf = ceres_df.loc[str(year)+'-'+str(month)+'-'+str(day):str(year)+'-'+str(month)+'-'+str(day)]['cldarea_total_1h'].values.tolist()
         cf = [i/100 for i in cf]
 
@@ -91,6 +97,6 @@ def main(dataset):
     fsds_corrected_values = df['fsds_corrected'].tolist()
     dataset['fsds_corrected'] = 'time', fsds_corrected_values
 
-    os.remove(stn + '.ceres.nc')
+    os.remove(stn + sfx)
 
     return dataset
